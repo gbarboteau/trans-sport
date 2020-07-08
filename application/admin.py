86d1446dc1ca5ad5b9d825abf1548defcc3b11d1
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-07-08 14:56:20 +0200

    bug fix

--- application/admin.py
+++ application/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django import forms
-from django.contrib.auth.models import Group
 from application.models import User, Category, Adress, Place, Comment, Profile
 
 
--- transsport_project/settings.py
+++ transsport_project/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # on utilise l'adaptateur postgresql
         'NAME': 'transsport', # le nom de notre base de donnees creee precedemment
-        'USER': 'gabrielle', # attention : remplacez par votre nom d'utilisateur
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'USER': 'Gabrielle', # attention : remplacez par votre nom d'utilisateur
+        'PASSWORD': os.environ.get('PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
