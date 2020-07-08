55059007ca5d72a890bd326e2dfad7cbfb940252
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-07-08 14:25:53 +0200

    Heroku bug fix

--- transsport_project/settings.py
+++ transsport_project/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'transsport_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # on utilise l'adaptateur postgresql
         'NAME': 'transsport', # le nom de notre base de donnees creee precedemment
         'USER': 'gabrielle', # attention : remplacez par votre nom d'utilisateur
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
