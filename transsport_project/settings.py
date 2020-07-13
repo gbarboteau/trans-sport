7eb3afd00b00b73c1e36fd0d9a65971a7cb383fe
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-07-14 01:20:40 +0200

    typos

--- transsport_project/settings.py
+++ transsport_project/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'transsport_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # on utilise l'adaptateur postgresql
+        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
         'NAME': 'transsport', # le nom de notre base de donnees creee precedemment
-        'USER': 'Gabrielle', # attention : remplacez par votre nom d'utilisateur
+        'USER': 'gabrielle', # attention : remplacez par votre nom d'utilisateur
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
