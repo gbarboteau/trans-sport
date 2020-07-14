d67dc7e2639633a4925c6e169a7fd8c2a6ce3d9c
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-07-14 07:24:15 +0200

    Adding 404 page + bug fix

--- transsport_project/settings/__init__.py
+++ transsport_project/settings/__init__.py
@@ -11,7 +11,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -145,6 +145,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-
-DEBUG_PROPAGATE_EXCEPTIONS = True
