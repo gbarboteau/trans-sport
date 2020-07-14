2cd5f076658a0f8635f1ac6ce03cd0f62c035a0e
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-07-14 04:42:52 +0200

    production bug fix and rebase

--- transsport_project/settings.py
+++ transsport_project/settings.py
@@ -25,9 +25,9 @@ GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 if os.environ.get('ENV') == 'PRODUCTION':
     DEBUG = False
 else:
-    DEBUG = True
+    DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'trans-sport.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'trans-sport.herokuapp.com', '127.0.0.1']
 
 
 
@@ -136,25 +136,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-
-# if os.environ.get('ENV') == 'PRODUCTION':
-
-# Static files settings
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-# # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
