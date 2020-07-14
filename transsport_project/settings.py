3702a10520313426ffb56a46b4b6d7ab41241cd2
Gabrielle Barboteau <g.barboteau@gmail.com>
2020-07-14 02:56:10 +0200

    typos

--- transsport_project/settings.py
+++ transsport_project/settings.py
@@ -147,15 +147,14 @@ STATICFILES_DIRS = [
 # if os.environ.get('ENV') == 'PRODUCTION':
 
 # Static files settings
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'static/application'),
-)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
